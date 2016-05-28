58cb84fde7991501525ad46d9a80ef49e7b0144f
podvysotskya <a_podvysotskya_@ukr.net>
2016-05-28 14:50:12 +0300

    Update pp.py

--- pp.py
+++ pp.py
@@ -7,7 +7,8 @@ rx=10
 ry=100
 rx2=540
 ry2=100
-i=0
+il=0
+ir=0
 p1=320
 p2=320
 p3=320
@@ -17,7 +18,7 @@ p6=320
 def setup():
     size(600,400)
 def draw():
-    global bx,by,bxs,bys,rx,ry,rx2,ry2,i,po
+    global bx,by,bxs,bys,rx,ry,rx2,ry2,i,il,ir,p1,p2,p3,p4,p5,p6
     fill(255,255,255)
     rect(0,0,800,800)
     fill(229,204,255)
@@ -27,8 +28,8 @@ def draw():
     triangle(255,310,315,310,285,280)
     triangle(255,10,315,10,285,40)
     fill(255,255,204)
-    rect(rx,ry,20,100)
-    rect(rx2,ry2,20,100)
+    rect(10,ry,20,100)
+    rect(540,ry2,20,100)
     fill(204,255,255)
     ellipse(bx,by,15,15)
     rect(10,p1,30,20)
@@ -51,7 +52,7 @@ def draw():
            ry2=ry2-2
        if key == 'l' and ry2<=209:
            ry2=ry2+2
-    if bx<=rx+20 and bx<=ry+100 and by>=rx and by>=ry:
+    if bx<=-30 and b
         bxs=-bxs
     if bx>=rx2 and bx>=ry2 and by<=rx2-20 and by<=ry2+100:
          bxs=-bxs
@@ -69,7 +70,15 @@ def draw():
         by=150
     if by>=297:
         bys=-bys
-    if bx<=60:
-         i+=1
-         po=800
-            
+    if bx<=19 and il==0:
+         il+=1
+         p1=800
+    if bx<=19 and il==1:
+         il+=1
+         p2=800
+    if bx<=19 and il==2:
+        il+=1
+        p3=800
+    if by>=0 and ir==60:
+        ir-=1
+        p4=800
